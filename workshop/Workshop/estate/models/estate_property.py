4792ec3bb3bbf696eb0370cca0a02865334fbd1d
Shyam Sundar Velmurugan <HwuShyam2005>
2026-09-07 18:28:03 +0400

    session 3 august

--- workshop/Workshop/estate/models/estate_property.py
+++ workshop/Workshop/estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import api, models, fields
+from odoo.exceptions import UserError
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
@@ -16,3 +17,20 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one('res.partner', string = 'Buyer')
     salesperson_id = fields.Many2one('res.users', string = 'Salesperson', default=lambda self: self.env.user)
     offer_ids = fields.One2many('estate.property.offer', 'property_id')
+    
+    state = fields.Selection(selection=[('new','New'), ('offer_accepted','Offer Accepted'), ('sold','Sold'), ('cancelled',' Cancelled')], default='new')
+    
+    def sell_property(self):
+        for record in self:
+            if record.state == 'cancelled':
+                raise UserError('you cannot sell a cancelled property')
+            else:
+                record.state = 'sold'
+                
+    def cancel_property(self):
+        for record in self:
+            if record.state == 'sold':
+                raise UserError('you cannot cancel a sold property')
+            else:
+                record.state = 'cancelled'
+                
--- workshop/Workshop/estate/models/estate_property_offer.py
+++ workshop/Workshop/estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
 from odoo import api,models,fields
+from odoo.exceptions import UserError
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -7,4 +8,28 @@ class EstatePropertyOffer(models.Model):
     price = fields.Float()
     partner_id = fields.Many2one('res.partner')
     
-    property_id = fields.Many2one('estate.property')
+    property_id = fields.Many2one('estate.property') 
+    
+    state = fields.Selection(selection = [('accepted', 'Accepted'), ('refused', 'Refused')])
+    
+    def accept_offer(self):
+        for record in self:
+            if record.property_id.state == 'offer_accepted':
+                raise UserError('this property already has an offer accepted')
+            elif record.state == 'refused':
+                raise UserError('you cannot accept a refused offer')
+            else:
+                 record.property_id.state = 'offer_accepted'
+                 record.property_id.buyer_id = record.partner_id
+                 record.property_id.selling_price = record.price
+                 record.state = 'accepted'
+    
+    def refuse_offer(self):
+        for record in self:
+            if record.state == 'accepted':
+                raise UserError('accepted offers cannot be refused')
+            else:
+                record.state = 'refused'
+                
+
+                
